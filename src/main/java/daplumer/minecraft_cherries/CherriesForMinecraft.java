380dfd2db8c556ef657bcb6c09b4b7d80c030184
DaPlumer <daplumer271@gmail.com>
2025-04-20 20:46:23 -0400

    Nests

--- src/main/java/daplumer/minecraft_cherries/CherriesForMinecraft.java
+++ src/main/java/daplumer/minecraft_cherries/CherriesForMinecraft.java
@@ -1,13 +1,18 @@
 package daplumer.minecraft_cherries;
 
+import daplumer.minecraft_cherries.block.Nest;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.loot.v3.LootTableEvents;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.piston.PistonBehavior;
 import net.minecraft.component.DataComponentTypes;
 import net.minecraft.component.type.ConsumableComponents;
 import net.minecraft.component.type.FoodComponents;
 import net.minecraft.enchantment.Enchantments;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.item.Items;
@@ -17,38 +22,47 @@ import net.minecraft.loot.condition.SurvivesExplosionLootCondition;
 import net.minecraft.loot.condition.TableBonusLootCondition;
 import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.registry.RegistryKey;
+import net.minecraft.sound.BlockSoundGroup;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+
+import static daplumer.minecraft_cherries.CherriesForMinecraftRegistries.*;
+
+@SuppressWarnings("unused")
 public class CherriesForMinecraft implements ModInitializer {
-	@SuppressWarnings("unused")
-    public static final String MOD_ID = "minecraft_cherries";
+    public static final  String MOD_ID = "minecraft_cherries";
+	public static Logger LOGGER = Logger.getLogger(MOD_ID);
+	public static final Item CHERRY = ITEMS.register("cherry",new Item.Settings().food(FoodComponents.APPLE));
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Item ORANGE = ITEMS.register("orange",new Item.Settings().food(FoodComponents.APPLE));
+	public static final Block NEST_BLOCK = BLOCKS.register("nest",AbstractBlock.Settings.create().breakInstantly().burnable().pistonBehavior(PistonBehavior.DESTROY).nonOpaque().sounds(BlockSoundGroup.GRASS), Nest::new);
+	public static final BlockItem NEST_ITEM = (BlockItem) ITEMS.register("nest",new Item.Settings(),BLOCK_ITEM(NEST_BLOCK));
+
+    public static final Map<RegistryKey<LootTable>, Item> droppedFruits = new HashMap<>();
+
+	public static final Item CHERRY_JUICE = ITEMS.register("cherry_juice", new Item.Settings()
+			.food(FoodComponents.HONEY_BOTTLE)
+			.component(DataComponentTypes.CONSUMABLE, ConsumableComponents.DRINK)
+			.useRemainder(Items.GLASS_BOTTLE));
+	public static final Item ORANGE_JUICE = ITEMS.register("orange_juice", new Item.Settings()
+			.food(FoodComponents.HONEY_BOTTLE)
+			.component(DataComponentTypes.CONSUMABLE, ConsumableComponents.DRINK)
+			.useRemainder(Items.GLASS_BOTTLE));
+
 	@SuppressWarnings("OptionalGetWithoutIsPresent")
-    public static final RegistryKey<LootTable> CHERRY_LEAVES_LOOT_TABLE_ID = Blocks.CHERRY_LEAVES.getLootTableKey().get();
-	public static final Item CHERRY = CherriesForMinecraftRegistries.ITEMS.register("cherry",new Item.Settings().food(FoodComponents.APPLE));
-
-	public static final Item ORANGE = CherriesForMinecraftRegistries.ITEMS.register("orange",new Item.Settings().food(FoodComponents.APPLE));
-
-
-	public static final Item CHERRY_JUICE = CherriesForMinecraftRegistries.ITEMS.register("cherry_juice", new Item.Settings()
-			.food(FoodComponents.HONEY_BOTTLE)
-			.component(DataComponentTypes.CONSUMABLE, ConsumableComponents.DRINK)
-			.useRemainder(Items.GLASS_BOTTLE));
-	public static final Item ORANGE_JUICE = CherriesForMinecraftRegistries.ITEMS.register("orange_juice", new Item.Settings()
-			.food(FoodComponents.HONEY_BOTTLE)
-			.component(DataComponentTypes.CONSUMABLE, ConsumableComponents.DRINK)
-			.useRemainder(Items.GLASS_BOTTLE));
-
-	@Override
+    @Override
 	public void onInitialize() {
+		droppedFruits.put(Blocks.CHERRY_LEAVES.getLootTableKey().get(),CHERRY);
+		droppedFruits.put(Blocks.JUNGLE_LEAVES.getLootTableKey().get(),ORANGE);
 		CherriesForMinecraftRegistries.Initialize();
 
 		ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(entries -> {
 			entries.addAfter(Items.APPLE,CHERRY);
+			entries.addAfter(Items.HONEY_BOTTLE,CHERRY_JUICE);
 
 			entries.addAfter(CHERRY,ORANGE);
-			entries.addAfter(Items.HONEY_BOTTLE,CHERRY_JUICE);
 
 			entries.addAfter(CHERRY_JUICE,ORANGE_JUICE);
 		});
@@ -56,13 +70,16 @@ public class CherriesForMinecraft implements ModInitializer {
 
 		//register loot tables for fruit
 		LootTableEvents.MODIFY.register(((key, tableBuilder, source, registries) -> {
-			if (source.isBuiltin() && CHERRY_LEAVES_LOOT_TABLE_ID.equals(key)){
+			if (source.isBuiltin() && droppedFruits.containsKey(key)){
 				LootPool.Builder poolBuilder = LootPool.builder()
-						.with(ItemEntry.builder(CHERRY)
+						.with(ItemEntry.builder(droppedFruits.get(key))
 								.conditionally(SurvivesExplosionLootCondition.builder())
 								.conditionally(TableBonusLootCondition.builder(registries.getEntryOrThrow(Enchantments.FORTUNE),0.005f,0.005555f,0.00625f,0.008333f,0.025f)));
 				tableBuilder.pool(poolBuilder);
 			}
 		}));
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.NATURAL).register((entries ->
+			entries.addAfter(Items.FIREFLY_BUSH,NEST_ITEM)
+		));
 	}
 }
---
+++ src/main/java/daplumer/minecraft_cherries/CherriesForMinecraftClient.java
@@ -0,0 +1,15 @@
+package daplumer.minecraft_cherries;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.RenderLayer;
+
+public class CherriesForMinecraftClient implements ClientModInitializer {
+    /**
+     * Runs the mod initializer on the client environment.
+     */
+    @Override
+    public void onInitializeClient() {
+        BlockRenderLayerMap.INSTANCE.putBlock(CherriesForMinecraft.NEST_BLOCK, RenderLayer.getCutout());
+    }
+}
